608ccf4b3f357d8ff9a6db2adfaba4305bd7df50
Grudzien <70582964+ProfGrudzien@users.noreply.github.com>
2022-09-20 22:50:10 +0200

    Delete Premiere directory

--- Premiere/test.py
+++
@@ -1,7 +0,0 @@
-seuil = int(input("Entier strictement positif : "))
-puissance = 1 
-exposant = 0 
-while puissance < seuil :
-    puissance = puissance * 2 
-    exposant = exposant + 1 
-print(seuil, "est compris entre 2 puissance", exposant - 1, "et 2 puissance", exposant)
